1a6b7466a8e8efc7b28249d6cefbb00307bfa0cf
denise baker <denviol@yahoo.com>
2020-03-03 18:43:16 -0600

    Add project files.

---
+++ calculator c-sharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace calculator_c_sharp
+{
+    class Program
+    {
+        public static object Multiply { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some numbers separated by commas with no spaces.");
+            var csv = Console.ReadLine();
+            var nums = csv.Split(',').Select(n => int.Parse(n)).ToArray();
+            
+
+            Console.WriteLine("Type m for multiply or s for square");
+            var response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "m":
+                    var finalProduct = 1;
+
+                    for (var i = 0; i < nums.Length; i++)
+                    {
+                        finalProduct = finalProduct * nums[i];
+                    }
+
+                    Console.WriteLine(finalProduct);
+                    break;
+
+                case "s":
+                    int squaredNumbers;
+                    var squaredNumberList = "";
+                    for (var i = 0; i < nums.Length; i++)
+                    {
+                        squaredNumbers = nums[i] * nums[i];
+                        squaredNumberList = $"{squaredNumbers}, ";
+                        Console.WriteLine(squaredNumberList);
+                    }
+                    break;
+            }
+
+        }
+    }
+}
+    
